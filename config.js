1a4db5035fa21628ce96d08a33cae8093673cef8
jeffyactive <jeff@reelyactive.com>
2018-08-19 18:44:03 -0400

    Simultaneous WiFi and BLE with raddec

--- config.js
+++ config.js
@@ -7,9 +7,8 @@
 // Begin configurable parameters
 // -----------------------------
 
-const WIFI_MAC_ADDRESS = '02:a3:51:62:27:b1';
-const TARGET_ADDRESS = 'pareto.reelyactive.com';
-const TARGET_PORT = 50000;
+const TARGET_ADDRESS = '192.168.0.100';
+const TARGET_PORT = 50001;
 const MAX_PAYLOAD_BYTES = 508;
 const MAX_DELAY_MILLISECONDS = 500;
 
@@ -17,7 +16,6 @@ const MAX_DELAY_MILLISECONDS = 500;
 // End configurable parameters
 
 
-module.exports.wifiMacAddress = WIFI_MAC_ADDRESS;
 module.exports.targetAddress = TARGET_ADDRESS;
 module.exports.targetPort = TARGET_PORT;
 module.exports.maxPayloadBytes = MAX_PAYLOAD_BYTES;
--- index.js
+++ index.js
@@ -7,34 +7,29 @@
 
 const tessel = require('tessel');
 const dgram = require('dgram');
-const reelay = require('reelay');
+const Barnowl = require('barnowl');
+const BarnowlReel = require('barnowl-reel');
+const BarnowlTcpdump = require('barnowl-tcpdump');
 const config = require('./config');
 const uartListener = require('./uartListener');
-const wifiMonitor = require('./wifiMonitor');
 
 // Create a UDP client
-var client = dgram.createSocket('udp4');
+let client = dgram.createSocket('udp4');
 
 // Listen on UART (Port A) and generate events from the data stream
-var uart = new uartListener('A');
+let uart = new uartListener('A');
 
-// Enable the relay
-var relay = new reelay();
-relay.addListener( { protocol: 'event', path: uart, enableMixing: false } );
-relay.addForwarder({
-  protocol: 'udp',
-  port: config.targetPort,
-  address: config.targetAddress,
-  maxPayloadBytes: config.maxPayloadBytes,
-  maxDelayMilliseconds: config.maxDelayMilliseconds
-});
+let barnowl = new Barnowl({ enableMixing: false });
 
-// Listen on WiFi and generate events from the data stream
-var wifi = new wifiMonitor();
+// Configure barnowl to listen for both reel and tcpdump
+barnowl.addListener(BarnowlReel, {}, BarnowlReel.EventListener,
+                    { path: uart });
+barnowl.addListener(BarnowlTcpdump, {}, BarnowlTcpdump.SpawnListener, {});
 
-// Forward the raddec via UDP -> TODO: move port/address to config
-wifi.on('raddec', function(packet) {
-  client.send(packet, 50001, '127.0.0.1', function(err) {
+// Forward the raddec via UDP
+barnowl.on('raddec', function(raddec) {
+  client.send(new Buffer(raddec.encodeAsHexString(), 'hex'), config.targetPort,
+              config.targetAddress, function(err) {
     if(err) {
       console.log('UDP forwarding error', err);
     }
