dec3b16e09c2e02259418041d05a34cf9e663ec9
jeffyactive <jeff@reelyactive.com>
2019-01-30 08:39:48 -0500

    Listen directly on Tessel UART, remove intermediary

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ const Barnowl = require('barnowl');
 const BarnowlReel = require('barnowl-reel');
 const BarnowlTcpdump = require('barnowl-tcpdump');
 const config = require('./config');
-const uartListener = require('./uartListener');
 
 const raddecOptions = {
     includePackets: true,
     includeTimestamp: true
 };
 
+const REEL_BAUD_RATE = 230400;
+
 // Create a UDP client
 let client = dgram.createSocket('udp4');
 
-// Listen on UART (Port A) and generate events from the data stream
-let uart = new uartListener('A');
+// Listen on UART (Port A) for reel data events
+let uart = new tessel.port['A'].UART({ baudrate: REEL_BAUD_RATE });
 
 // Configure barnowl to listen for both reel and tcpdump
 let barnowl = new Barnowl({ enableMixing: true });
@@ -30,11 +31,13 @@ barnowl.addListener(BarnowlReel, {}, BarnowlReel.EventListener,
                     { path: uart });
 barnowl.addListener(BarnowlTcpdump, {}, BarnowlTcpdump.SpawnListener, {});
 
-// Forward the raddec via UDP
+// Forward the raddec via UDP and pulse the green LED
 barnowl.on('raddec', function(raddec) {
+  tessel.led[2].on();
   let raddecHex = raddec.encodeAsHexString(raddecOptions);
   client.send(new Buffer(raddecHex, 'hex'), config.targetPort,
               config.targetAddress, function(err) { });
+  tessel.led[2].off();
 });
 
 // Blue LED continuously toggles to indicate program is running
